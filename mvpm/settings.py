7f34ee72e43bb7c1aa966e8b33626f76a211dbe1
NanduSrinivas <nandusrinivas33.cse@gmail.com>
2017-05-26 16:32:41 +0530

    connected MySql Database

--- mvpm/settings.py
+++ mvpm/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'mvpm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mvpm',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWD': '',
     }
 }
 
