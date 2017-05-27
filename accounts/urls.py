d4ee213a63dda421c419805c607959710261fcf2
NanduSrinivas <nandusrinivas33.cse@gmail.com>
2017-05-27 21:44:00 +0530

    prevented Ananymous users with "login_required"

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     url(r'^login/$',views.login_view,name='login'),
     url(r'^logout/$',views.logout_view,name='logout'),
     url(r'^dashboard/$',views.dashboard,name='dashboard'),
+    url(r'dashboard/news_updates/$',views.newsupdates,name='newsupdates'),
+    url(r'dashboard/billing/$',views.billing,name='billing'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import (
     login,
     logout,
 )
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
@@ -18,6 +19,7 @@ def home(request):
 
 def login_view(request):
     print(request.user.is_authenticated())
+    next = request.GET.get('next') #incase of login required
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
         username = form.cleaned_data.get("username")
@@ -25,6 +27,8 @@ def login_view(request):
         user = authenticate(username=username,password=password)
         login(request,user)
         print(request.user.is_authenticated())
+        if next:
+            return redirect(next)
         return redirect('/account/dashboard')
     return render(request,'accounts/login.html',{"form":form})
 
@@ -32,5 +36,15 @@ def logout_view(request):
     logout(request)
     return redirect('/account/login')
 
+@login_required(login_url='/account/login/')
 def dashboard(request):
     return render(request,'dashboard/dashboard.html',{})
+
+@login_required(login_url='/account/login/')
+def newsupdates(request):
+    return render(request,'dashboard/news&updates.html',{})
+
+@login_required(login_url='/account/login/')
+def billing(request):
+    return render(request,'dashboard/billing.html',{})
+
