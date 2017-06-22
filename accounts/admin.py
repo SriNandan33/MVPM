797f6da3e28802449e9b2f62a23eac551c1c1441
NanduSrinivas <nandusrinivas33.cse@gmail.com>
2017-06-22 20:26:42 +0530

    customized modeladmin for userprofile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user","email","phone"]
+    search_fields = ["user__username"]
+    class Meta:
+        model = UserProfile
+
+admin.site.register(UserProfile,UserProfileAdmin)
