ffa9f18794e8eb9a49b236ad8fc594f34b7d3350
NanduSrinivas <nandusrinivas33.cse@gmail.com>
2017-06-22 19:43:55 +0530

    customized maintenancerequest page in admin

--- promanage/admin.py
+++ promanage/admin.py
@@ -3,4 +3,11 @@ from . import models
 
 admin.site.register(models.Property)
 admin.site.register(models.Notification)
-admin.site.register(models.MaintenanceRequest)
+
+class MaintenanceRequestAdmin(admin.ModelAdmin):
+    list_display = ["user","issue_title","is_serviced"]
+    list_filter = ["is_serviced","date"]
+    search_fields = ["user__username",]
+    class Meta:
+       model = models.MaintenanceRequest
+admin.site.register(models.MaintenanceRequest,MaintenanceRequestAdmin)
